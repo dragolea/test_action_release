aaaddd1c893ff58531297f7180699c2bbd8b7a11
Mathias von Kaiz <127110360+mvk-abs@users.noreply.github.com>
2024-09-26 08:24:31 +0200

    Fix/local dev (#18)
    * chore: updated npm package versions
    
    * perf: removed cdn loading of SAPUI5 resources
    
    * fix: combined local frontend dev, TypeScript and deployment to BTP

---
+++ srv/server.js
@@ -0,0 +1,34 @@
+// HACK: temporary hack to enable local frontend development together with deployment to BTP -> move this in a CAP plugin.
+// This is due to a mismatch of the manifest.json odata path (relative/absolute).
+// A working deployment needs a relative path for odata service in manifest.json
+// which interferes with the TypeScript frontend development based on the CAP cds-plugin-ui5.
+// This plugin injects the app path in the service url which leads to a 404 in loading metadata.
+
+// TODO: 
+// - change to cap plugin
+// - get all services paths dynamically (e.g. service ServiceAccruals @(path: '/accruals'))
+// - get all apps
+// - replace the url string dynamically for all app/service combinations
+// - The cds-plugin-ui5 removes automatically the webapp folder but can be configured in cds package.json part like
+/* - modules are the related frontend UI5 apps in ./app folder
+  "cds": {  
+    "cds-plugin-ui5": {
+      "modules": {
+        "fcoaccruals": {
+          "mountPath": "/fcoaccruals"
+        }
+      }
+    }
+  }
+*/
+import cds from '@sap/cds';
+
+cds.on('bootstrap', app => {
+  app.use(function(req, res, next) {
+    req.url = req.url.replace('/fcoaccruals/accruals/', '/accruals/');
+    res.setHeader('Rewrite-Path', req.url);
+    next();
+  });
+});
+
+export default cds.server;
